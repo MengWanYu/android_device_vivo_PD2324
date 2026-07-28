14e1b2e4f8357d94680b81ebb53f7d982483e7c2
MengWanYu <131175837+MengWanYu@users.noreply.github.com>
2026-07-29 01:17:19 +0800

    fix(BoardConfig): add BUILD_BROKEN_MISSING_REQUIRED_MODULES to skip missing AIDL NDK libs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,7 @@ DEVICE_PATH := device/vivo/PD2324
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 
 # A/B
 AB_OTA_UPDATER := true
